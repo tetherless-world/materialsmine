20d74f56b94b93d5d29087a5e326d8f1c6e6dcb6
Tristan Protzman <tlprotzman+git@gmail.com>
2019-07-24 18:55:00 +0000

    change import, add run_agent

--- tests/template.py
+++ tests/template.py
@@ -1,5 +1,5 @@
 from . import ingest_tester
-from testcase import WhyisTestCase
+from whyis_test_case import WhyisTestCase
 
 
 class IngestTestSetup(WhyisTestCase):
@@ -11,6 +11,29 @@ class IngestTestSetup(WhyisTestCase):
 
     def setUp(self):
         ingest_tester.setUp(self, self.file_under_test)
+
+    def run_agent(self, agent, nanopublication=None):
+        app = self.app
+        agent.dry_run = True
+        agent.app = app
+        results = []
+        if nanopublication is not None:
+            results.extend(agent.process_graph(nanopublication))
+        elif agent.query_predicate == app.NS.whyis.globalChangeQuery:
+            results.extend(agent.process_graph(app.db))
+        else:
+            print("Running as update agent")
+            for resource in agent.getInstances(app.db):
+                print(resource.identifier)
+                for np_uri, in app.db.query('''select ?np where {
+    graph ?assertion { ?e ?p ?o.}
+    ?np a np:Nanopublication;
+        np:hasAssertion ?assertion.
+}''', initBindings={'e': resource.identifier}, initNs=app.NS.prefixes):
+                    print(np_uri)
+                    np = app.nanopub_manager.get(np_uri)
+                    results.extend(agent.process_graph(np))
+        return results
 
 class IngestTestTests(IngestTestSetup):  
     def test_nanocomposites(self):
