43044035503ce00ecd377baebc18d382b6b80860
Minor Gordon <gordom6@rpi.edu>
2019-07-26 17:03:26 -0400

    try a set -e

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+set -e
+
 cd /apps/whyis
 python3 manage.py test --ci
-
-if [ ! -f "test-results/py/results.xml" ]; then
-    exit 1
-fi
-
-cat test-results/py/results.xml
 
 if [ "$(grep -c "failure "test-results/py/results.xml)" -ge 1 ]; then
     exit 1
