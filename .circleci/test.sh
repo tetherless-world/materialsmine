6ae4036b784280ef2edb172dfb610e064d3b96d3
Minor Gordon <gordom6@rpi.edu>
2019-07-26 17:21:47 -0400

    fix grep

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -5,7 +5,7 @@ set -e
 cd /apps/whyis
 python3 manage.py test --ci
 
-if [ "$(grep -c "failure "test-results/py/results.xml)" -ge 1 ]; then
+if [ "$(grep -c 'failure ' test-results/py/results.xml)" -ge 1 ]; then
     exit 1
 else
     exit 0
