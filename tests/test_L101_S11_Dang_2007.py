be751e4e0373dbb9574a50828048719bc1f7d4f4
Jim McCusker <mccusker@gmail.com>
2020-07-15 13:30:21 -0400

    Initialize sabbir_proc

---
+++ tests/test_L101_S11_Dang_2007.py
@@ -0,0 +1,15 @@
+from . import ingest_tester
+from . import template
+
+file_under_test = "L101_S11_Dang_2007"
+
+class IngestTestRunner(template.IngestTestSetup):
+    first_run = bool()
+    @classmethod
+    def setUpClass(cls):
+        cls.file_under_test = file_under_test
+        super().setUpClass()
+
+    def test_triples(self):
+         ingest_tester.print_triples(self)
+
