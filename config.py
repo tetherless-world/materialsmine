af8b030d9199013906b20e6fb594423a0a714246
Tolulomo <tolulomofateye@gmail.com>
2020-10-06 04:31:12 -0400

    retyped whyis unit converter import because it failed at deploy

--- config.py
+++ config.py
@@ -22,14 +22,14 @@ skos = rdflib.Namespace("http://www.w3.org/2004/02/skos/core#")
 
 from nanomine.agent import *
 
-import whyis_unit_converter.unit_converter_agent as converter
 
 from authenticator import JWTAuthenticator
+import whyis_unit_converter.unit_converter_agent as converter
 
 authenticator_config = [] # set into config dict later
 authenticator_secret = os.environ.get('NM_GRAPH_AUTH_SECRET', None)
 if authenticator_secret:
-authenticator_config.append(JWTAuthenticator(key=authenticator_secret))
+    authenticator_config.append(JWTAuthenticator(key=authenticator_secret))
 
 # base config class; extend it to your needs.
 Config = dict(
